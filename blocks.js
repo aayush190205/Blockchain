10829e997ca3982e4f2ca8d7fa329bd7045f8e8d
aayush190205 <aayushojha1902@gmail.com>
2025-09-25 09:47:43 +0530

    Update blocks.js

--- blocks.js
+++ blocks.js
@@ -1,8 +1,21 @@
 
-
 window.addEventListener('load', async () => {
     const contractAddress = "0x7B33669604a0904801a686291332CBE6C068835e";
-    const contractABI = [ { "inputs": [], "stateMutability": "nonpayable", "type": "constructor" }, { "anonymous": false, "inputs": [ { "indexed": false, "internalType": "bytes32", "name": "", "type": "bytes32" }, { "indexed": false, "internalType": "string", "name": "", "type": "string" }, { "indexed": false, "internalType": "string", "name": "", "type": "string" } ], "name": "CertificateIssued", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": false, "internalType": "bytes32", "name": "", "type": "bytes32" } ], "name": "CertificateRevoked", "type": "event" }, { "inputs": [ { "internalType": "address", "name": "", "type": "address" } ], "name": "authorized", "outputs": [ { "internalType": "bool", "name": "", "type": "bool" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "name": "certificateIdList", "outputs": [ { "internalType": "bytes32", "name": "", "type": "bytes32" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "bytes32", "name": "", "type": "bytes32" } ], "name": "certificates", "outputs": [ { "internalType": "string", "name": "studentName", "type": "string" }, { "internalType": "string", "name": "courseName", "type": "string" }, { "internalType": "uint256", "name": "issueDate", "type": "uint256" }, { "internalType": "bool", "name": "isRevoked", "type": "bool" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "getCertificateCount", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "bytes32", "name": "certificateId", "type": "bytes32" } ], "name": "getCertificateDetails", "outputs": [ { "internalType": "string", "name": "", "type": "string" }, { "internalType": "string", "name": "", "type": "string" }, { "internalType": "uint256", "name": "", "type": "uint256" }, { "internalType": "bool", "name": "", "type": "bool" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "address", "name": "_newUser", "type": "address" } ], "name": "grantAuthority", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "string", "name": "_studentName", "type": "string" }, { "internalType": "string", "name": "_courseName", "type": "string" } ], "name": "issueCertificate", "outputs": [ { "internalType": "bytes32", "name": "", "type": "bytes32" } ], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "owner", "outputs": [ { "internalType": "address", "name": "", "type": "address" } ], "stateMutability": "view", "type": "function" }, { "inputs": [ { "internalType": "bytes32", "name": "certificateId", "type": "bytes32" } ], "name": "revokeCertificate", "outputs": [], "stateMutability": "nonpayable", "type": "function" } ];
+    const contractABI = [
+        { "inputs": [], "stateMutability": "nonpayable", "type": "constructor" },
+        { "anonymous": false, "inputs": [ { "indexed": false, "internalType": "bytes32", "name": "", "type": "bytes32" }, { "indexed": false, "internalType": "string", "name": "", "type": "string" }, { "indexed": false, "internalType": "string", "name": "", "type": "string" } ], "name": "CertificateIssued", "type": "event" },
+        { "anonymous": false, "inputs": [ { "indexed": false, "internalType": "bytes32", "name": "", "type": "bytes32" } ], "name": "CertificateRevoked", "type": "event" },
+        { "inputs": [ { "internalType": "address", "name": "", "type": "address" } ], "name": "authorized", "outputs": [ { "internalType": "bool", "name": "", "type": "bool" } ], "stateMutability": "view", "type": "function" },
+        { "inputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "name": "certificateIdList", "outputs": [ { "internalType": "bytes32", "name": "", "type": "bytes32" } ], "stateMutability": "view", "type": "function" },
+        { "inputs": [ { "internalType": "bytes32", "name": "", "type": "bytes32" } ], "name": "certificates", "outputs": [ { "internalType": "string", "name": "studentName", "type": "string" }, { "internalType": "string", "name": "courseName", "type": "string" }, { "internalType": "uint256", "name": "issueDate", "type": "uint256" }, { "internalType": "bool", "name": "isRevoked", "type": "bool" } ], "stateMutability": "view", "type": "function" },
+        { "inputs": [], "name": "getCertificateCount", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" },
+        { "inputs": [ { "internalType": "bytes32", "name": "certificateId", "type": "bytes32" } ], "name": "getCertificateDetails", "outputs": [ { "internalType": "string", "name": "", "type": "string" }, { "internalType": "string", "name": "", "type": "string" }, { "internalType": "uint256", "name": "", "type": "uint256" }, { "internalType": "bool", "name": "", "type": "bool" } ], "stateMutability": "view", "type": "function" },
+        { "inputs": [ { "internalType": "address", "name": "_newUser", "type": "address" } ], "name": "grantAuthority", "outputs": [], "stateMutability": "nonpayable", "type": "function" },
+        { "inputs": [ { "internalType": "string", "name": "_studentName", "type": "string" }, { "internalType": "string", "name": "_courseName", "type": "string" } ], "name": "issueCertificate", "outputs": [ { "internalType": "bytes32", "name": "", "type": "bytes32" } ], "stateMutability": "nonpayable", "type": "function" },
+        { "inputs": [], "name": "owner", "outputs": [ { "internalType": "address", "name": "", "type": "address" } ], "stateMutability": "view", "type": "function" },
+        { "inputs": [ { "internalType": "bytes32", "name": "certificateId", "type": "bytes32" } ], "name": "revokeCertificate", "outputs": [], "stateMutability": "nonpayable", "type": "function" }
+    ];
+
     const rpcUrl = "https://sepolia.infura.io/v3/5cb8eb30eaae446c81ab26a22e968dda"; 
     
     const blocksContainer = document.getElementById('blocks-container');
@@ -19,16 +32,32 @@ window.addEventListener('load', async () => {
         const date = new Date(block.timestamp * 1000).toLocaleString();
         const myTxs = block.myTransactions || [];
 
-        let txDetailsHTML = '';
+        // Certificate txs
+        let certTxsHTML = '';
         if (myTxs.length > 0) {
-            txDetailsHTML += `<div class="block-field"><span class="field-label">Your Certificate Transactions in this Block:</span>`;
+            certTxsHTML += `<div class="block-field"><span class="field-label">Your Certificate Transactions in this Block:</span>`;
             myTxs.forEach(tx => {
-                // The event arguments for the simplified contract are certId, studentName, courseName
-                txDetailsHTML += `<div class="field-value" style="margin-left: 20px;">- Certificate for <b>${tx.args[1]}</b> (${tx.args[2]})</div>`;
+                certTxsHTML += `<div class="field-value" style="margin-left: 20px;">- Certificate for <b>${tx.args[1]}</b> (${tx.args[2]})</div>`;
+            });
+            certTxsHTML += `</div>`;
+        }
+
+        // Normal transactions
+        let txDetailsHTML = '';
+        if (block.transactions.length > 0) {
+            txDetailsHTML += `<div class="block-field"><span class="field-label">Transactions:</span>`;
+            block.transactions.forEach(tx => {
+                txDetailsHTML += `
+                  <div class="field-value" style="margin-left:20px;">
+                    Hash: ${tx.hash}<br>
+                    From: ${tx.from}<br>
+                    To: ${tx.to || "Contract Creation"}<br>
+                    Value: ${ethers.utils.formatEther(tx.value)} ETH
+                  </div>`;
             });
             txDetailsHTML += `</div>`;
         }
-        
+
         blockDiv.innerHTML = `
              <div class="block-header">Block #${block.number}</div>
                 <div class="block-field">
@@ -44,14 +73,11 @@ window.addEventListener('load', async () => {
                     <span class="field-value">${date}</span>
                 </div>
                 <div class="block-field">
-                    <span class="field-label">Transactions:</span>
-                    <span class="field-value">${block.transactions.length}</span>
-                </div>
-                <div class="block-field">
                     <span class="field-label">Mined by:</span>
                     <span class="field-value">${block.miner}</span>
                 </div>
             ${txDetailsHTML}
+            ${certTxsHTML}
         `;
         return blockDiv;
     };
@@ -73,7 +99,7 @@ window.addEventListener('load', async () => {
             const latestBlockNumber = await provider.getBlockNumber();
             const blockPromises = [];
             for (let i = 0; i < 15 && (latestBlockNumber - i >= 0); i++) {
-                blockPromises.push(provider.getBlock(latestBlockNumber - i));
+                blockPromises.push(provider.getBlockWithTransactions(latestBlockNumber - i));
             }
             allBlocksCache = await Promise.all(blockPromises);
 
@@ -106,4 +132,3 @@ window.addEventListener('load', async () => {
 
     fetchData();
 });
-
